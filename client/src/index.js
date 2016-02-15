fdbbd2d7bfa2708ceb740df7da82c9b1e8b060b8
User <user@Users-MacBook-Pro-7.local>
2016-02-15 16:03:34 +0000

    testing onload of portfolio

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,25 @@ var _ = require('lodash');
 
 window.onload = function(){
 
-  console.log(sampleStocks);
-  console.log('lodash', _);
+  // console.log(sampleStocks);
+  
+
+  createShares = function(){
+      portfolioShareArray = [];
+      for(object of sampleStocks){
+        var onloadShare new Share(object.name, object.epic, object.price);
+        onloadShare.closingPrice = object.pastCloseOfDayPrices;
+        var portfolioShareInfo = {name:object.name, share:onloadShare, quantity:object.quantity, date:object.buyDate, avgPurchasePrice:Object.buyPrice}
+        portfolioShareArray.push(portfolioShareInfo);
+      }
+      var jaysMumsPortfolio = new Portfolio("Jay's Mum", 1000)
+      jaysMumsPortfolio.sharePortfolio = portfolioShareArray;
+      console.log("Our Portfolio====", jaysMumsPortfolio);
 }
+
+    createShares();
+
+}
+
+
 
