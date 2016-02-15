0b13cf68665bfcf1b5d4410f9e68767ba2625df9
User <user@Users-MacBook-Pro-7.local>
2016-02-15 16:43:58 +0000

    Displaying in the display

--- client/src/index.js
+++ client/src/index.js
@@ -1,34 +1,34 @@
 // var Bank = require("./bank/bank.js");
 var sampleStocks = require('./sampleStockData.json');
 var _ = require('lodash');
-//var Account = require("./bank/account.js")
+var Share = require('./models/share.js')
+var Portfolio = require('./models/portfolio.js')
 
 
 
+  createShares = function(){
+      portfolioShareArray = [];
+      for(object of sampleStocks){
+        var onloadShare = new Share(object.name, object.epic, object.price);
+        onloadShare.closingPrice = object.pastCloseOfDayPrices;
+        var portfolioShareInfo = {name:object.name, share:onloadShare, quantity:object.quantity, date:object.buyDate, avgPurchasePrice:object.buyPrice}
+        portfolioShareArray.push(portfolioShareInfo);
+      }
+      var MumsPortfolio = new Portfolio("Mum", 1000)
+      MumsPortfolio.sharePortfolio = portfolioShareArray;
+      return MumsPortfolio;
+}
 
-
+share1 = new Share("Insleys", "INS", 120.00)
 
 
 
 window.onload = function(){
 
-  // console.log(sampleStocks);
-  
-
-  createShares = function(){
-      portfolioShareArray = [];
-      for(object of sampleStocks){
-        var onloadShare new Share(object.name, object.epic, object.price);
-        onloadShare.closingPrice = object.pastCloseOfDayPrices;
-        var portfolioShareInfo = {name:object.name, share:onloadShare, quantity:object.quantity, date:object.buyDate, avgPurchasePrice:Object.buyPrice}
-        portfolioShareArray.push(portfolioShareInfo);
-      }
-      var jaysMumsPortfolio = new Portfolio("Jay's Mum", 1000)
-      jaysMumsPortfolio.sharePortfolio = portfolioShareArray;
-      console.log("Our Portfolio====", jaysMumsPortfolio);
-}
-
-    createShares();
+    var portfolio = createShares();
+    portfolio.addCash(2000);
+    portfolio.buyShares(share1, 1)
+    console.log(portfolio);
 
 }
 
