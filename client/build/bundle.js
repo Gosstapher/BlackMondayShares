2c8400a5cd9e5854ec080ea56d7b34c052f03393
User <user@Users-MacBook-Pro-7.local>
2016-02-16 09:37:50 +0000

    portfolio view working

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -47,12 +47,15 @@
 	// var Bank = require("./bank/bank.js");
 	var sampleStocks = __webpack_require__(1);
 	var _ = __webpack_require__(2);
-	var Share = __webpack_require__(4)
-	var Portfolio = __webpack_require__(103)
+	var Share = __webpack_require__(4);
+	var Portfolio = __webpack_require__(103);
+	var PortfolioView = __webpack_require__(104);
+	
+	share1 = new Share("Insleys", "INS", 120.00)
+	portfolioView = new PortfolioView();
 	
 	
-	
-	  createShares = function(){
+	  createShares = function(callback){
 	      portfolioShareArray = [];
 	      for(object of sampleStocks){
 	        var onloadShare = new Share(object.name, object.epic, object.price);
@@ -60,21 +63,20 @@
 	        var portfolioShareInfo = {name:object.name, share:onloadShare, quantity:object.quantity, date:object.buyDate, avgPurchasePrice:object.buyPrice}
 	        portfolioShareArray.push(portfolioShareInfo);
 	      }
-	      var MumsPortfolio = new Portfolio("Mum", 1000)
+	      var MumsPortfolio = new Portfolio("Mum's", 1000)
 	      MumsPortfolio.sharePortfolio = portfolioShareArray;
-	      return MumsPortfolio;
+	       MumsPortfolio;
+	       callback(MumsPortfolio);
 	}
 	
-	share1 = new Share("Insleys", "INS", 120.00)
 	
 	
 	
 	window.onload = function(){
 	
-	    var portfolio = createShares();
-	    portfolio.addCash(2000);
-	    portfolio.buyShares(share1, 1)
-	    console.log(portfolio);
+	    createShares(portfolioView.createView);
+	    
+	
 	
 	}
 	
@@ -27892,6 +27894,63 @@
 	
 	module.exports = Portfolio
 
+
+/***/ },
+/* 104 */
+/***/ function(module, exports) {
+
+	
+	
+	
+	var PortfolioView = function(){
+	
+	  this.createView = function(portfolio){
+	    //Get the total Values of the Portfolio
+	    var holderName = document.querySelector("#holderName");
+	    holderName.innerHTML = "<h2 class='title'>" + portfolio.holder + " Share Portfolio</h2>";
+	
+	    for(shareObject of portfolio.sharePortfolio){
+	      var portfolioContainer = document.querySelector("#mainPortfolioContainer")
+	      var shareRow = document.createElement("div");
+	      shareRow.id = shareObject.name
+	      //Share Name Display
+	      var nameHolder = document.createElement("p");
+	      nameHolder.innerText ="Company Name: " + shareObject.name;
+	      shareRow.appendChild(nameHolder);
+	      //Share epic Display
+	      var epicHolder = document.createElement("p");
+	      epicHolder.innerText = "Share identifer: " + shareObject.share.epic;
+	      shareRow.appendChild(epicHolder);
+	      //Share Current Price Display
+	      var priceHolder = document.createElement("p");
+	      priceHolder.innerText = "Current Share Price: " + shareObject.share.price + " pence";
+	      shareRow.appendChild(priceHolder);
+	      //Quantity of shares held
+	      var quantityHolder = document.createElement("p");
+	      quantityHolder.innerText ="Quantity Held: " + shareObject.quantity;
+	      shareRow.appendChild(quantityHolder);
+	      //Display Buy Price
+	      var percentageChange = ((shareObject.share.price - shareObject.avgPurchasePrice)/(shareObject.avgPurchasePrice)) * 100;
+	      var buyHolder = document.createElement("div");
+	      buyHolder.innerHTML = "<p>Buy Price: " + shareObject.avgPurchasePrice + "pence <br> Percentage Change: " + Math.round(percentageChange) + "% </p>" ;
+	      var percentView = document.querySelector(".percentageChange")
+	        if(percentageChange > 0){
+	          buyHolder.style.color = "green"
+	        }else{
+	          buyHolder.style.color = "red"
+	        }
+	      shareRow.appendChild(buyHolder);
+	      portfolioContainer.appendChild(shareRow);
+	      shareRow.style.borderTop = "thick solid #0000FF";
+	    }//End of Table Build
+	    var portfolioInfo = document.querySelector("#portfolioInfo")
+	    var portfolioCurrentValue = document.createElement("h3")
+	    portfolioCurrentValue.innerText ="Total Assets in Portfolio: £" + ((portfolio.getCurrentValue().totalAssets)/100) + "          Invested in Shares: £" + ((portfolio.getCurrentValue().shareValue)/100) + "          Cash Holdings: £" + ((portfolio.getCurrentValue().cashTotal)/100);
+	    portfolioInfo.appendChild(portfolioCurrentValue);
+	  }//End of Create View Method
+	}//end of model
+	
+	module.exports = PortfolioView;
 
 /***/ }
 /******/ ]);
