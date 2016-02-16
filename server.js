3844d8927d00a586b577397e28bad75316d69e6f
nickhillwd <nickhillwd@gmail.com>
2016-02-16 16:12:37 +0000

    server connection added for mongoMOdel shares

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Shares = require('./mongoModels/share.js');
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.use(express.static('client/build'));
+mongoose.connect('mongodb://localhost/black-monday');
 
+Shares.find(function(err, shares) {
+  if(err) console.log(err)
+  app.locals.shares = shares;
+});
+
+app.use(express.static('client/build'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// GET REQUEST TO SHARE DB
+// app.get("/", function(req, res){
+//   Shares.find(function(err, shares) {
+//     if(err) console.log(err)
+//     res.('index', { animals: animals });
+//   })
+// });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
