d0e9af6485b2faa9335f0b212bcc06af0c406c97
nickhillwd <nickhillwd@gmail.com>
2016-02-16 10:18:03 +0000

    pulling uptodate development

--- client/src/models/api.js
+++ client/src/models/api.js
@@ -1,8 +1,3 @@
-//get input
-//get json
-//format json
-//create share from formatted json
-
 var apiModel = {
   get: function(epic, callback){
     var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + epic +"%22)&env=store://datatables.org/alltableswithkeys&format=json";
